4b12da5d92b498d4a9dfd83402ac7cc10cfefe29
hujia <hujia031@pingan.com.cn>
2019-06-26 16:44:37 +0800

    非常规代码规范

---
+++ pot-gold-web/src/main/java/com/blankhouse/project/potgold/brilliant/NoMVC.java
@@ -0,0 +1,11 @@
+package com.blankhouse.project.potgold.brilliant;
+
+/**
+ * @author hujia
+ * @date 2019/6/26
+ * @since 1.0.0
+ *
+ * @apiNote 非常规，非模块化，自由设计的代码，写在这个地方
+ */
+public class NoMVC {
+}
