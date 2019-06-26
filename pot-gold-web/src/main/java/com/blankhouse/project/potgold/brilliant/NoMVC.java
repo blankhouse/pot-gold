b66b87e2ee1908cb4be7d75dc1e7a46d2ff1affe
hujia <hujia031@pingan.com.cn>
2019-06-26 22:27:19 +0800

    format

--- pot-gold-web/src/main/java/com/blankhouse/project/potgold/brilliant/NoMVC.java
+++ pot-gold-web/src/main/java/com/blankhouse/project/potgold/brilliant/NoMVC.java
@@ -5,7 +5,7 @@ package com.blankhouse.project.potgold.brilliant;
  * @date 2019/6/26
  * @since 1.0.0
  *
- * @apiNote 非常规，非模块化，自由设计的代码，写在这个地方
+ * @apiNote 非常规，非Service,DAO，是自由设计的代码！写在这个地方！
  */
 public interface NoMVC {
 }
